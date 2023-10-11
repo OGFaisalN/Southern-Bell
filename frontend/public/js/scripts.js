90f88cf386b84c8d9eb80022da57674867f438fa
Faisal N <contact@faisaln.com>
2023-10-11 17:40:49 -0400

    prep for authors, minimize article-i

--- frontend/public/js/scripts.js
+++ frontend/public/js/scripts.js
@@ -5,3 +5,20 @@ window.addEventListener('scroll', () => {
         document.querySelector('.toTop').classList.remove('visible');
     };
 });
+
+function copyLink() {
+    const el = document.createElement('textarea');
+    el.value = window.location.href;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false; el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    if (selected) {
+        document.getSelection().removeAllRanges();
+        document.getSelection().addRange(selected);
+    };
+    alert('Link copied!');
+};
