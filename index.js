a504baabe4694cc70e3f27d476c181e520cb32c2
Faisal <contact@faisaln.com>
2024-10-21 18:39:54 +0000

    Fix up author page logic

--- index.js
+++ index.js
@@ -293,14 +293,11 @@ async function startApp() {
 
     app.get('/authors/:author', async (req, res) => {
         await allRoutes(req, res);
-        var author = cms.authors.find(author => (author.name === req.params.author.replaceAll('+', ' ') && (!author.unlisted)));
-        var articles = cms.articles.filter(article => article.author === req.params.author.replaceAll('+', ' ')).filter(article => !article.unlisted);
-        var artworks = cms.artworks.filter(artwork => artwork.author === req.params.author.replaceAll('+', ' ')).filter(article => !article.unlisted);
-        if ((articles.length > 0) || (artworks.length > 0)) {
-            res.render('author', { vars: defaults, title: req.params.author.replaceAll('+', ' '), cms, pageviews: req.pageViews, author, articles, artworks });
-        } else {
-            res.render('404', { vars: defaults, title: '404', cms, pageviews: req.pageViews });
-        };
+        var author = cms.authors.find(author => (author.name.toLowerCase() === req.params.author.replaceAll('-', ' ').toLowerCase()) && (!author.unlisted));
+        var articles = cms.articles.filter(article => article.author && (article.author.toLowerCase() === req.params.author.replaceAll('-', ' ').toLowerCase()) && !article.unlisted);
+        var artworks = cms.artworks.filter(artwork => artwork.author && (artwork.author.toLowerCase() === req.params.author.replaceAll('-', ' ').toLowerCase()) && !artwork.unlisted);
+        if ((articles.length > 0) || (artworks.length > 0)) return res.render('author', { vars: defaults, title: (articles[0] || artworks[0]).author, cms, pageviews: req.pageViews, author, articles, artworks });
+        return res.render('404', { vars: defaults, title: '404', cms, pageviews: req.pageViews });
     });
 
     app.get('/search', async (req, res) => {
